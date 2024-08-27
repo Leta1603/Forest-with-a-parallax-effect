e0b539d60855c7a5fe82a416140750c203f13cec
Leta1603 <dawa1603@mail.ru>
2024-08-27 22:31:58 +0300

    Editing touchmove

--- js/app.js
+++ js/app.js
@@ -8,10 +8,11 @@ document.addEventListener("mousemove", (e) => {
 });
 
 document.addEventListener("touchmove", (e) => {
+    console.log("e.clientX", e.touches[0].clientX);
     Object.assign(document.documentElement, {
         style: `
-        --move-x:${(e.clientX - window.innerWidth / 2) * -0.007}deg;
-        --move-y:${(e.clientY - window.innerHeight / 2) * -0.02}deg;
+        --move-x:${(e.touches[0].clientX - window.innerWidth / 2) * -0.007}deg;
+        --move-y:${(e.touches[0].clientY - window.innerHeight / 2) * -0.02}deg;
         `,
     });
 });
